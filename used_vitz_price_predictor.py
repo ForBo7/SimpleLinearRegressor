a7e26481e0ba78a60145fb806d96ab25623b3fc0
ForzBox7 <salmananaqvii+github@gmail.com>
2022-02-12 21:24:58 +0500

    Added code to simulate reading the data.

--- used_vitz_price_predictor.py
+++ used_vitz_price_predictor.py
@@ -8,3 +8,24 @@ model_year, kms_driven, price,
 2010, 40_000, 1_250_000,
 2000, 250_000, 765_000,
 """
+
+# Arbitrary sets of coefficients.
+coefficients = [
+    [1, 2],
+    [2, 2],
+    [2, 3],
+]
+
+# Read in data.
+file = StringIO(data)
+data_samples = []
+
+for sample in file:
+    listed_sample = sample.split(",")
+    # Remove new line.
+    del listed_sample[-1]
+    data_samples.append(listed_sample)
+
+# Remove headers.
+del data_samples[0]
+del data_samples[0]
