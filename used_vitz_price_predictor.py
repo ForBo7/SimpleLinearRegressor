eaf6d1b01aad0cff980f1719210142a4efdb5cc5
ForzBox7 <salmananaqvii+github@gmail.com>
2022-02-14 21:35:40 +0500

    Implemented least mean squares algorithm.

--- used_vitz_price_predictor.py
+++ used_vitz_price_predictor.py
@@ -1,4 +1,5 @@
 from io import StringIO
+from random import randint
 
 data = """
 model_year, kms_driven, price,
@@ -10,10 +11,10 @@ model_year, kms_driven, price,
 """
 
 # Arbitrary sets of coefficients.
-coefficients = [
-    [1, 2],
-    [2, 2],
-    [2, 3],
+coefficient_sets = [
+    [randint(10, 20), randint(5, 20)],
+    [randint(10, 20), randint(5, 20)],
+    [randint(10, 20), randint(5, 20)],
 ]
 
 # Read in data.
@@ -29,3 +30,26 @@ for sample in file:
 # Remove headers.
 del data_samples[0]
 del data_samples[0]
+
+# Create a list of actual prices.
+actual_prices = []
+for data_sample in data_samples:
+    actual_prices.append(int(data_sample[-1]))
+
+# Calculate least mean squares.
+least_mean_squares = []
+for coefficient_set in coefficient_sets:
+    predicted_prices = []
+    # Calculate predicted prices.
+    for data_sample in data_samples:
+        predicted_prices.append((coefficient_set[0] * int(data_sample[0])) +
+                                (coefficient_set[1] * int(data_sample[1])))
+    # Find difference between predicted prices and actual prices.
+    zip_object = zip(predicted_prices, actual_prices)
+    differences = [predicted_price - actual_price
+                   for predicted_price, actual_price in zip_object]
+    differences_squared = [difference ** 2 for difference in differences]
+    least_mean_squares.append(sum(differences_squared))
+least_mean_square = min(least_mean_squares)
+best_coefficient_set = coefficient_sets[
+    least_mean_squares.index(least_mean_square)]
